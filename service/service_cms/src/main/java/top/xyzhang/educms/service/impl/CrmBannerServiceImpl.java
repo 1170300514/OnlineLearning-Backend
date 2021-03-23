9437d9e8fe33591a16bef98f451303bcbbe922ac
Haoyu <xyzhang.top@foxmail.com>
2021-03-23 10:25:52 +0800

    简单整合Redis(未进行application.properties配置)

--- service/service_cms/src/main/java/top/xyzhang/educms/service/impl/CrmBannerServiceImpl.java
+++ service/service_cms/src/main/java/top/xyzhang/educms/service/impl/CrmBannerServiceImpl.java
@@ -1,11 +1,11 @@
 package top.xyzhang.educms.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 import top.xyzhang.educms.entity.CrmBanner;
 import top.xyzhang.educms.mapper.CrmBannerMapper;
 import top.xyzhang.educms.service.CrmBannerService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -20,6 +20,11 @@ import java.util.List;
 @Service
 public class CrmBannerServiceImpl extends ServiceImpl<CrmBannerMapper, CrmBanner> implements CrmBannerService {
 
+    /**
+     * 查询全部Banner信息
+     * @return
+     */
+//    @Cacheable(key = "'selectIndexList'",value = "banner") // Redis缓存
     @Override
     public List<CrmBanner> getAllBanner() {
         // 根据id降序排列 取前两条
